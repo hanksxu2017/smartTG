54003be033cbb0dc90c132782fc092c78b873c8c
xuwenyi <xuwenyi@senrong.com>
2018-12-04 17:40:24 +0800

    1. 增加教室租赁信息

---
+++ smart-web2/src/main/web/cn/com/smart/web/controller/impl/StudyClassroomRentalController.java
@@ -0,0 +1,94 @@
+package cn.com.smart.web.controller.impl;
+
+import cn.com.smart.bean.SmartResponse;
+import cn.com.smart.constant.IConstant;
+import cn.com.smart.web.bean.RequestPage;
+import cn.com.smart.web.bean.entity.TGStudyClassroom;
+import cn.com.smart.web.bean.entity.TGStudyClassroomRental;
+import cn.com.smart.web.bean.entity.TGStudySchool;
+import cn.com.smart.web.bean.entity.TNDict;
+import cn.com.smart.web.bean.search.ClassroomSearch;
+import cn.com.smart.web.controller.base.BaseController;
+import cn.com.smart.web.service.*;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/studyClassroom/rental")
+public class StudyClassroomRentalController extends BaseController {
+
+    @Autowired
+    private OPService opService;
+
+    @Autowired
+    private StudyClassroomService classroomService;
+
+    @Autowired
+    private StudyClassroomRentalService classroomRentalService;
+
+    public StudyClassroomRentalController() {
+        super.setSubDir("/studyClassroom/rental/");
+    }
+
+    /**
+     * @param searchParam
+     * @param page
+     * @return
+     */
+    @RequestMapping("/list")
+    public ModelAndView list(ClassroomSearch searchParam, RequestPage page) {
+        SmartResponse<Object> smartResp = opService.getDatas("classroom_rental_list", searchParam, page.getStartNum(), page.getPageSize());
+        return this.packListModelView(searchParam, smartResp);
+    }
+
+	@Autowired
+	private DictService dictService;
+
+    /**
+     *
+     * @return
+     */
+    @RequestMapping(value = "/add")
+    public ModelAndView add() {
+        ModelAndView modelView = new ModelAndView();
+
+        modelView.getModelMap().put("classrooms", classroomService.findNormal().getDatas());
+
+	    modelView.getModelMap().put("courseTimes", dictService.getItems("COURSE_TIMES").getDatas());
+
+	    modelView.getModelMap().put("weekInfoList", dictService.getItems("WEEK_INFO_LIST").getDatas());
+
+        modelView.setViewName(getPageDir() + "add");
+        return modelView;
+    }
+
+    @RequestMapping(value = "/getIdleCourseTime", method = RequestMethod.POST)
+    @ResponseBody
+    public SmartResponse<TNDict> getIdleCourseTime(String classroomId, int weekInfo) {
+		// TODO
+
+	    return null;
+    }
+
+	/**
+	 * 提交新增
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@ResponseBody
+	public SmartResponse<String> save(TGStudyClassroomRental classroomRental) {
+		classroomRental.setCreateTime(new Date());
+		return classroomRentalService.save(classroomRental);
+	}
+
+}
---
+++ smart-web2/src/main/web/cn/com/smart/web/dao/impl/StudyClassroomRentalDao.java
@@ -0,0 +1,22 @@
+package cn.com.smart.web.dao.impl;
+
+import cn.com.smart.dao.impl.BaseDaoImpl;
+import cn.com.smart.res.SQLResUtil;
+import cn.com.smart.res.sqlmap.SqlMapping;
+import cn.com.smart.web.bean.entity.TGStudyClassroom;
+import cn.com.smart.web.bean.entity.TGStudyClassroomRental;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+/**
+ * 版本DAO
+ * @author XUWENYI <br />
+ * 2018年11月15日
+ * @version 1.0
+ * @since 1.0
+ */
+@Repository
+public class StudyClassroomRentalDao extends BaseDaoImpl<TGStudyClassroomRental> {
+
+}
